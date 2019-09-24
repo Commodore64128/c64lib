6f647888f47b0d0006525f689bf897788fb734c3
Bruce Sutherland <bsutherland@users.noreply.github.com>
2019-09-24 21:33:49 +0900

    Joystick works on top of CIA.

--- cia.asm
+++ cia.asm
@@ -2,6 +2,10 @@
 
 .const CIA_IRQ_CONTROL = $dc0d
 .const CIA_NMI_CONTROL = $dd0d
+.const CIA_PORT_A = $dc00
+.const CIA_PORT_B = $dc01
+.const CIA_DDR_PORT_A = $dc02
+.const CIA_DDR_PORT_B = $dc03
 
 .macro cia_irq_disable() {
     lda #$7f
--- joystick.asm
+++ joystick.asm
@@ -1,9 +1,14 @@
 #importonce
+#import "cia.asm"
 
 // confusingly, joystick 1 is on port B
-.const JOYSTICK1_PORT = $dc01
+.const JOYSTICK1_PORT = CIA_PORT_B
+.const JOYSTICK1_DDR = CIA_DDR_PORT_B
+
 // joystick 2 is on port A
-.const JOYSTICK2_PORT = $dc00
+.const JOYSTICK2_PORT = CIA_PORT_A
+.const JOYSTICK2_DDR = CIA_DDR_PORT_A
+
 
 // bitmasks for joystick switches
 .const JOYSTICK_UP = $01
